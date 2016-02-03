35d840b7c4994ae6313b5a6e4f518aad0f382a19
Ben Adams <thundercat@illyriad.co.uk>
2016-02-03 02:25:07 +0000

    System.Buffers in StreamHelperExtensions

--- src/Microsoft.AspNetCore.WebUtilities/StreamHelperExtensions.cs
+++ src/Microsoft.AspNetCore.WebUtilities/StreamHelperExtensions.cs
@@ -1,6 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Buffers;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,13 +12,14 @@ namespace Microsoft.AspNetCore.WebUtilities
     {
         public static async Task DrainAsync(this Stream stream, CancellationToken cancellationToken)
         {
-            byte[] buffer = new byte[1024];
+            var buffer = ArrayPool<byte>.Shared.Rent(1024);
             cancellationToken.ThrowIfCancellationRequested();
             while (await stream.ReadAsync(buffer, 0, buffer.Length, cancellationToken) > 0)
             {
                 // Not all streams support cancellation directly.
                 cancellationToken.ThrowIfCancellationRequested();
             }
+            ArrayPool<byte>.Shared.Return(buffer);
         }
     }
 }
